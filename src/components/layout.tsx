51970046aae3119b10ec75526fc9c8d90aed64b1
jonjau <jonjau3Q@gmail.com>
2021-01-28 02:30:00 +1100

    deleted unused code, updated README

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./navbar";
 
 import styled from "styled-components";
-import { spacing, fontSize } from "../common";
 import Container from "./container";
 
 export const LayoutBackground = styled.div`
@@ -20,21 +19,6 @@ export const LayoutHeader = styled.header`
 export const LayoutMain = styled.main`
   display: grid;
   grid-template-columns: 1fr 2fr 1fr;
-`;
-
-export const GlobalWrapper = styled.div`
-  margin: ${spacing[20]} auto;
-  max-width: 52rem;
-  padding: ${spacing[5]};
-`;
-
-export const GlobalHeader = styled.div`
-  margin-bottom: ${spacing[2]};
-`;
-
-export const MainHeading = styled.h2`
-  font-size: ${fontSize[5]};
-  margin: ${spacing[0]};
 `;
 
 interface Props {
